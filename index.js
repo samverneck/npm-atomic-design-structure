7054b5ead0fafc6ff1acb60ff0271a10ddac09ad
SAMUEL VERNECK <osamucaverneck@gmail.com>
2017-02-02 18:12:20 -0200

    projeto

---
+++ index.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+fs.mkdir('_atoms');
+fs.mkdir('_organelles');
+fs.mkdir('_quarks');
+fs.mkdir('_factories');
+fs.mkdir('_hadrons');
+fs.mkdir('_config');
+fs.mkdir('modules');
+
